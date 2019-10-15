148eec96a4cfe8e8e6c4cb77a7f52274fdf2ffcb
Klifford Agujar <forddagujar95@gmail.com>
2019-10-15 11:54:53 -0400

    Assignment (#3)
    * Test build
    
    * i think i forgot to install packages
    
    * Its a trap
    
    * pls work

--- webpack.config.js
+++ webpack.config.js
@@ -9,8 +9,7 @@ module.exports = {
   entry: './src/js/index.js',
   output: {
     filename: 'main.js',
-    path: path.resolve(__dirname, 'public'),
-    publicPath: '/'
+    path: path.resolve(__dirname, 'public')
   },
   mode: devMode ? 'development' : 'production',
   module: {
